7c759e91921fa1ac17f6532731147e3e425cc0af
aamirajmeri <132576353+aamirajmeri@users.noreply.github.com>
2024-04-24 09:27:38 +0000

    Day 2
    Band name generator

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-name = input("What's your name? ")
-length = len(name)
-print(length)
+print("Welcome to the Band Name Generator.")
+street = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + street + " " + pet)
