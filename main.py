12a73ae51b85758bcfa1aa39d1811701342806a5
aamirajmeri <132576353+aamirajmeri@users.noreply.github.com>
2024-04-26 11:17:45 +0000

    Day 2 Added
    data types convert Int into String.

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-print("Welcome Bro in band Name generator")
-city = input("Enter your grow up city name")
-pet = input("Enter your pet name\n")
-print("your band name is " + city + " " + pet)
+# num_char = len(input("What's your name ?"))
+# #print(type(num_char))
+# new_num_char = str(num_char)
+# print("Your name has " + new_num_char + " characters.")
+
+# a = int(848)
+# print(type(a))
+
+# print(80 + float(111.5))
+print(str(80) + str(100))
