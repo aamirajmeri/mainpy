9d9fd0c0fdf5a9845d258a4479936bd1d40fd8b0
aamirajmeri <132576353+aamirajmeri@users.noreply.github.com>
2024-05-06 15:19:12 +0000

    update
    commit

--- main.py
+++ main.py
@@ -20,6 +20,22 @@
 # x = int(input("Enter your name"))
 # print(type(x))
 
-l_side = 4
-r_side = 2
-l_side / r_side
+
+#####.......Operators ***
+
+# l_side = 4
+# r_side = 2
+# l_side / r_side
+
+
+
+
+#******** Date ******
+
+from datetime import date
+date.today()
+# datetime.date(2024, 5, 6)
+print(date.today())
+# 2024-05-06
+print("Today's date is:" + str(date.today()))
+# Today's date is:2024-05-06
