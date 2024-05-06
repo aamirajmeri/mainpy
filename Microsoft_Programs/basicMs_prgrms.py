fa517b4f121167076507025d48bae8e2edce0ace
aamirajmeri <132576353+aamirajmeri@users.noreply.github.com>
2024-05-06 17:00:50 +0000

    update
    commit

--- Microsoft_Programs/basicMs_prgrms.py
+++ Microsoft_Programs/basicMs_prgrms.py
@@ -17,6 +17,42 @@
 # print(a)
 
 
+#####.......Operators ***
+
+# l_side = 4
+# r_side = 2
+# l_side / r_side
+
+
+
+
+#******** Date ******
+
+# from datetime import date
+# date.today()
+# datetime.date(2024, 5, 6)
+# print(date.today())
+# 2024-05-06
+# print("Today's date is:" + str(date.today()))
+# Today's date is:2024-05-06
+
+
+
+# parsecs = 11
+# lightyears = parsecs * 3.26
+# print(f"{parsecs} +  your parsecs is  + {lightyears} + the lightyears is")
+#                 OR
+# parsecs = 11
+# lightyears = parsecs * 3.26
+# print(str(parsecs) + " parsecs is " + str(lightyears) + " lightyears")
+
+
+# import sys
+
+# print(sys.argv)
+# print(sys.argv[0]) # program name
+# print(sys.argv[1]) # first arg
+
 
 
 
--- main.py
+++ main.py
@@ -21,21 +21,3 @@
 # print(type(x))
 
 
-#####.......Operators ***
-
-# l_side = 4
-# r_side = 2
-# l_side / r_side
-
-
-
-
-#******** Date ******
-
-from datetime import date
-date.today()
-# datetime.date(2024, 5, 6)
-print(date.today())
-# 2024-05-06
-print("Today's date is:" + str(date.today()))
-# Today's date is:2024-05-06
